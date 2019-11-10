48c31dfd3940c30a626816ef333f5063156f48b0
Decosan <junx0401@gmail.com>
2019-11-10 16:01:45 +0700

    add:scoreインデックスページレイアウト

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,14 +1,26 @@
 class ScoresController < ApplicationController
+  before_action :set_score, only:[:show,:edit,:update,:destroy]
+
   def index
+    @scores = Score.all.order('created_at DESC').page(params[:page])
   end
 
   def show
   end
 
   def new
+    @score = Score.new
   end
 
   def create
+    @score = current_user.scores.build(score_params)
+    if @score.save
+      flash[:success] ='Create New score!'
+      redirect_to scores_path
+    else
+      flash[:danger] ='Failed..'
+      render 'new'
+    end
   end
 
   def edit
@@ -18,5 +30,15 @@ class ScoresController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def score_params
+    params.require(:score).permit(:in_score,:out_score,:total_score,:in_put,:out_put,:total_put)
+  end
+
+  def set_score
+    @score = Score.find(params[:id])
   end
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,11 @@
 class Score < ApplicationRecord
   belongs_to :course
   belongs_to :user
+
+  validates :in_score, presence: true
+  validates :out_score, presence: true
+  validates :total_score, presence: true
+  validates :in_put, presence: true
+  validates :out_put, presence: true
+  validates :total_put, presence: true
 end
